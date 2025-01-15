2c38bbd8ee26c510dea622c7d1a4841b9f192648
Arkadeep Ghosh <arkadeep_ghosh@rocklab.in>
2025-01-16 04:39:56 +0530

    Update buymeacoffee_auth.py

--- src/st_paywall/buymeacoffee_auth.py
+++ src/st_paywall/buymeacoffee_auth.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 def extract_payer_emails(json_response):
     payer_emails = []
-
+    print(json_response)
     for item in json_response["data"]:
         payer_email = item["payer_email"]
         payer_emails.append(payer_email)
